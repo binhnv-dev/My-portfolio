bf530a5e0c0849206b340dad18e3c6106494995f
Binhnv <binh46607@gmail.com>
2025-06-03 22:41:39 +0700

    update favicon

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Portfolio of Nguyen Van Binh - Software Developer',
+  icons: {
+    icon: '/favicon.png',
+  },
   description:
     'This is the portfolio of Nguyen Van Binh. I am a front end developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
 };
